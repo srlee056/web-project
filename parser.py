2a5c2c6ba392bb7c706cfef88b734f6760cb33f3
Lee Sarah <imsolem1226@gmail.com>
2019-12-07 21:50:08 +0900

    update - get each genre's event - get event date

--- parser.py
+++ parser.py
@@ -7,37 +7,46 @@ urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certifica
 위와같은 오류가 생겨서 ssl import 함 (구글 검색으로 해결법 찾음)
 '''
 
+
 fifteenList = []
 someList = []
 saleList = []
 hotdealList = []
 
-genre = ['romance', 'fantasy', 'bl']
+genre = ['romance']#, 'fantasy', 'bl']
 ser = '_serial'
 # zip 써서 각 장르별로 받아올 예정
-# 
-for i in range(1,10):
-    url = 'https://ridibooks.com/event/romance?type=ago&page='+str(i)
-    html = urlopen(url, context=context)
-    bsObject = BeautifulSoup(html.read(), "html.parser")
-    romanceEventList = bsObject.find_all('li', {'class':'event_list'})
 
-    for event in romanceEventList :
-        eventDesc = event.find('h3',{'class':'event_title'}).find('a')
-        text = eventDesc.text.strip() # strip() 함수는 공백 제거 목적
-        link = eventDesc.get('href')
-        if link.startswith('/'):
-            link = ''.join(["https://ridibooks.com",link])
-        
-        if '십오야' in text:
-            fifteenList.append({text:link})
-        elif 'SOME' in text:
-            someList.append({text:link})
-            #할인권 쓰는 썸딜은 모바일 전용인듯?
-        elif '%' in text:
-            saleList.append({text:link})
-        elif 'HOT DEAL' in text:
-            hotdealList.append({text:link})
+# 두 리스트의 조합을 구하는데 더 나은 방법이 있을까?
+# from itertools import product
 
-for e in fifteenList+someList+saleList+hotdealList:
-    print(e)
+for g in genre:
+    for i in range(1,3):
+        url = 'https://ridibooks.com/event/'+g+'?type=ago&page='+str(i)
+        html = urlopen(url, context=context)
+        bsObject = BeautifulSoup(html.read(), "html.parser")
+        romanceEventList = bsObject.find_all('li', {'class':'event_list'})
+
+        for event in romanceEventList :
+            eventDesc = event.find('h3',{'class':'event_title'}).find('a')
+            text = eventDesc.text.strip() # strip() 함수는 공백 제거 목적
+            link = eventDesc.get('href')
+            if link.startswith('/'):
+                link = ''.join(["https://ridibooks.com",link])
+            eventDate = event.find('li',{'class':'contents_descript'}).find('span',{'class':'descript_body'}).text.strip()
+            print(eventDate)
+            if '십오야' in text:
+                fifteenList.append({text:link})
+            elif 'SOME' in text:
+                someList.append({text:link})
+                #할인권 쓰는 썸딜은 모바일 전용인듯?
+            elif '%' in text:
+                saleList.append({text:link})
+            elif 'HOT DEAL' in text:
+                hotdealList.append({text:link})
+    
+for e in fifteenList :
+    url = e.link
+
+#for e in fifteenList+someList+saleList+hotdealList:
+#    print(e)
