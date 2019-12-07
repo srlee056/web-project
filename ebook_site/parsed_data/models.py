e082aa1e757fb2404f504777793c139600cfe25e
Lee Sarah <imsolem1226@gmail.com>
2019-12-08 00:09:50 +0900

    update - change the name showed on the admin page from ebookData(xx) to title.

--- ebook_site/parsed_data/models.py
+++ ebook_site/parsed_data/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class ebookData(models.Model):
     title = models.CharField(max_length=200)
     link = models.URLField()
+    def __str__(self):
+        return self.title
 
 class eventData(models.Model):
     title = models.CharField(max_length=200)
