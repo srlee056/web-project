8b1032a2bc7ccb44f773e73964f35d32c7a2f423
Lee Sarah <imsolem1226@gmail.com>
2019-12-17 23:12:51 +0900

    UPDATE - use enum to indicate event pages and event types well - check if event title has specific word and store them with adequate type enum. - store event and ebook instances(?) to intermediate model (EventLog)

--- ebook_site/parser.py
+++ ebook_site/parser.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+
+import enum
 import ssl
 context = ssl._create_unverified_context()
 '''
@@ -7,12 +9,25 @@ urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certifica
 위와같은 오류가 생겨서 ssl import 함 (구글 검색으로 해결법 찾음)
 '''
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ebook_site.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "ebook_site.settings")
 
 import django
 django.setup()
+from parsed_data.models import EbookData, EventData, EventLog
 
-from parsed_data.models import ebookData
+class Page(enum.IntEnum):
+    PRE = 0
+    PAST = 5
+
+class EventType(enum.IntEnum):
+    FT = 0 #fifthteen
+    SOME = 1
+    SALE = 2 #percent sale
+    HD = 3 #hotdeal
+
+PP = ['']
+for i in range(1, Page.PAST+1):
+    PP.append('?type=ago&page='+str(i))
 
 fifteenList = []
 someList = []
@@ -21,55 +36,58 @@ hotdealList = []
 
 genre = ['romance']#, 'fantasy', 'bl']
 ser = '_serial'
+
+
+# 현재, 지난 이벤트들의 주소를 위한 리스트.
+
 # zip 써서 각 장르별로 받아올 예정
 
 # 두 리스트의 조합을 구하는데 더 나은 방법이 있을까?
 # from itertools import product
 
+def getEventsMetadata():
+    data = {}
+    for g in genre:
+        for i in range(1, Page.PAST+1):
+            url = 'https://ridibooks.com/event/'+g+PP[i]
+            html = urlopen(url, context=context)
+            bsObject = BeautifulSoup(html.read(), "html.parser")
+            eventList = bsObject.select(
+                '#page_event_list > ul.event_list_wrapper > li'
+            )
 
-for g in genre:
-    for i in range(1,2):
-        url = 'https://ridibooks.com/event/'+g+'?type=ago&page='+str(i)
-        html = urlopen(url, context=context)
-        bsObject = BeautifulSoup(html.read(), "html.parser")
-        romanceEventList = bsObject.find_all('li', {'class':'event_list'})
-
-        for event in romanceEventList :
-            eventDesc = event.find('h3',{'class':'event_title'}).find('a')
-            text = eventDesc.text.strip() # strip() 함수는 공백 제거 목적
-            link = eventDesc.get('href')
-            if link.startswith('/'):
-                link = ''.join(["https://ridibooks.com",link])
-            eventDate = event.find('li',{'class':'contents_descript'}).find('span',{'class':'descript_body'})
-            #parsing date to datetime https://stackoverflow.com/questions/8636760/parsing-a-datetime-string-into-a-django-datetimefield/24228410
-            #print(eventDate)
-            if '십오야' in text:
-                fifteenList.append({text:link})
-            elif 'SOME' in text:
-                someList.append({text:link})
-                #할인권 쓰는 썸딜은 모바일 전용인듯?
-            elif '%' in text:
-                saleList.append({text:link})
-            elif 'HOT DEAL' in text:
-                hotdealList.append({text:link})
-
-#for e in fifteenList :
-#    url = e.link
+            for event in eventList :
+                eventDesc = event.find('h3',{'class':'event_title'}).find('a')
+                text = eventDesc.text.strip() # strip() 함수는 공백 제거 목적
+                link = eventDesc.get('href')
+                if link.startswith('/'):
+                    link = ''.join(["https://ridibooks.com",link])
+                date = event.find('li',{'class':'contents_descript'}).find('span',{'class':'descript_body'}).text.strip()
+                #parsing date to datetime https://stackoverflow.com/questions/8636760/parsing-a-datetime-string-into-a-django-datetimefield/24228410
+                
+                if '십오야' in text:
+                    t = EventType.FT
+                elif 'SOME' in text:
+                    t = EventType.SOME
+                elif '%' in text:
+                    t = EventType.SALE
+                elif 'HOT DEAL' in text:
+                    t = EventType.HD
+                else:
+                    continue
+                data[text] = (link, date, g, t)
+    return data
 
 #method that get metadata of books from event page. 
-def getBooksMetadata ():
-    booklist = []
-    url2 = 'https://ridibooks.com/event/18237'
-    html2 = urlopen(url2, context=context)
-    bsObject2 = BeautifulSoup(html2.read(), "html.parser")
+def getBooksMetadata (url):
+    #url = 'https://ridibooks.com/event/18237'
+    html = urlopen(url, context=context)
+    bsObject = BeautifulSoup(html.read(), "html.parser")
 
-    subgroup = bsObject2.find_all('div', {'id':'subgroup1'})
-    for g in subgroup:
-        #books = g.find_all('h3', {'class':'meta_title'}).
-        books = g.select(
-            'h3 > a'
-        )
-        booklist +=books
+    booklist = bsObject.select(
+        '#subgroup1 > div > div.book_metadata_wrapper > h3 > a'
+    )
+    #print(booklist)
 
     data = {}
     for b in booklist:
@@ -78,8 +96,25 @@ def getBooksMetadata ():
     return data
 
 if __name__=='__main__':
-    dataDict = getBooksMetadata()
-    for t, l in dataDict.items():
-        ebookData(title = t, link=l).save()
-#for e in fifteenList+someList+saleList+hotdealList:
-#    print(e)
+    eventDataDict = getEventsMetadata()
+    for t, (l, d, g, ty) in eventDataDict.items():
+        eD = EventData(title = t, link = l, date = d, genre=g, eType = ty)
+        eD.save()
+
+        ebookDataDict = getBooksMetadata(l)
+        for t, l in ebookDataDict.items():
+            bD = EbookData(title = t, link=l)
+            bD.save()
+
+            EventLog(event=eD, ebook=bD).save()
+
+
+
+    '''
+    dictionary 에 튜플로 value를 넣는것보다
+    dict list를 만들고
+    data[name] = ''
+    data[link] = 'https:// ..'
+    이렇게 하는게 나으려나...
+
+    '''
